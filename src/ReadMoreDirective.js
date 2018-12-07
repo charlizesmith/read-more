52fcf74398607d4ade67d160e56de499dfd22700
charlizesmith <charlizesmithweb@gmail.com>
2018-12-07 17:02:59 +0530

    Update ReadMoreDirective.js

--- src/ReadMoreDirective.js
+++ src/ReadMoreDirective.js
@@ -10,21 +10,21 @@ export default {
 	    read_more.href = '#';
 	    read_more.text = 'read more';
 
-	    var read_less = document.createElement('a');
-	    read_less.href = '#';
-	    read_less.text = 'read less';
+//	    var read_less = document.createElement('a');
+//	    read_less.href = '#';
+//	    read_less.text = 'read less';
 
 		  vn.elm.append(' ', read_more);
 
 			read_more.addEventListener("click", function(){ 
 				vn.elm.textContent = val_container;
-				vn.elm.append(' ', read_less);
+//				vn.elm.append(' ', read_less);
 			});
 
-			read_less.addEventListener("click", function(){ 
-				vn.elm.textContent = bind.value.substring(0,bind.arg)
-				vn.elm.append(' ', read_more);
-			});
+//			read_less.addEventListener("click", function(){ 
+//				vn.elm.textContent = bind.value.substring(0,bind.arg)
+//				vn.elm.append(' ', read_more);
+//			});
 
 		}else{
 			vn.elm.textContent = bind.value
